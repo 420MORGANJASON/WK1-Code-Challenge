18d25e4ce866185a216ced0f11d24759611965f0
MORGANJASON <420morganjason@gmail.com>
2023-03-20 09:35:58 +0300

    Changed the first function

--- netSalaryCalculator.js
+++ netSalaryCalculator.js
@@ -9,110 +9,111 @@ and benefits.
   MONTHLY RATES
  */
 // When the netSalaryCalculator is declared it checks the month and annual rates 
-function netSalaryCalculator(monthtax) {
+function taxCalculator(grosssalary) {
 
+let tax;
     // checks the monthtax and prints out the rates
-    if (monthtax <= 24000) {
-        return print = "Rate:10"
-    } else if (monthtax >= 24001 && monthtax <= 32333) {
-      return print = "Rate:20"
-    } else if (monthtax > 32333) {
-        return print = "Rate:30"
+    if (grosssalary <= 24000) {
+       tax = grosssalary * 0.1;
+    } else if (grosssalary<= 32333) {
+      tax = grosssalary * 0.25;
+    } else if (grosssalary > 32333) {
+       tax = grosssalary * 0.3;
     }
-     
+   return tax ; 
 }
-console.log(netSalaryCalculator(70000));
+console.log(taxCalculator(70000));
 
 
 
    
-//   ANNUAL RATES
+// //   ANNUAL RATES
 
 
-// When the netSalaryCalculator is declared it checks the annual rates 
+// // When the netSalaryCalculator is declared it checks the annual rates 
 
-function netSalaryCalculator(annualtax) {
+// function netSalaryCalculator(annualtax) {
 
-     // checks the annualtax and prints out the rates
+//      // checks the annualtax and prints out the rates
 
-    if (annualtax <= 288000) {
-        return print = "Rate:10"
-    } else if (annualtax >= 288001 && annualtax <=388000) {
-        return print = "Rate:20"
-    } else if (annualtax >388000) {
-        return print = "Rate:30"
-    }
-}
-console.log(netSalaryCalculator(300000));
-
-
-// Other Monthly PAYE parameters
+//     if (annualtax <= 288000) {
+//         return print = "Rate:10"
+//     } else if (annualtax >= 288001 && annualtax <=388000) {
+//         return print = "Rate:20"
+//     } else if (annualtax >388000) {
+//         return print = "Rate:30"
+//     }
+// }
+// console.log(netSalaryCalculator(300000));
 
 
-function netSalaryCalculator(monthlmt) {
-
-// Personal Relief
-  if (monthlmt === "ksh 2400") {
-    return print = "Effective since 25 April,2020";
-    // Insurance Relief
-  } else if (monthlmt === "ksh 5000") {
-    return print = "Effective since 1 January 2007";
-    // Allowable Pension Fund Contribution
-  } else if (monthlmt === "ksh 20000") {
-    return print = "Effective since 1 January,2006";
-    // Allowable HOSP Contribution
-  } else if (monthlmt === "ksh 0") {
-    return print = "Effective since 1 January,2021";
-    // Affordable Housing Relief
-  } else if (monthlmt === "ksh 9000") {
-    return print = "Effective since 1 January 2018";
-    // Allowable Owner Occupier Interest
-  } else if (monthlmt === "ksh 25000") {
-    return print = "Effective since 1 January,2017";
-    // Disabilitu Exemption
-  } else if (monthlmt === "ksh 150000") {
-    return print = "Effective since 10 March,2018";
-  }
-}
-  console.log(netSalaryCalculator("ksh 25000"));
+// // Other Monthly PAYE parameters
 
 
-// Other Annually PAYE parameters 
+// function netSalaryCalculator(monthlmt) {
+
+// // Personal Relief
+//   if (monthlmt === "ksh 2400") {
+//     return print = "Effective since 25 April,2020";
+//     // Insurance Relief
+//   } else if (monthlmt === "ksh 5000") {
+//     return print = "Effective since 1 January 2007";
+//     // Allowable Pension Fund Contribution
+//   } else if (monthlmt === "ksh 20000") {
+//     return print = "Effective since 1 January,2006";
+//     // Allowable HOSP Contribution
+//   } else if (monthlmt === "ksh 0") {
+//     return print = "Effective since 1 January,2021";
+//     // Affordable Housing Relief
+//   } else if (monthlmt === "ksh 9000") {
+//     return print = "Effective since 1 January 2018";
+//     // Allowable Owner Occupier Interest
+//   } else if (monthlmt === "ksh 25000") {
+//     return print = "Effective since 1 January,2017";
+//     // Disabilitu Exemption
+//   } else if (monthlmt === "ksh 150000") {
+//     return print = "Effective since 10 March,2018";
+//   }
+// }
+//   console.log(netSalaryCalculator("ksh 25000"));
+
+
+// // Other Annually PAYE parameters 
   
-// When the netSalaryCalculator is declared it checks the other PAYE parameters    
+// // When the netSalaryCalculator is declared it checks the other PAYE parameters    
 
-function netSalaryCalculator(annuallmt) {
+// function netSalaryCalculator(annuallmt) {
 
  
-// Personal Relief
-  if (annuallmt === "ksh 28800") {
-    return print = "Effective since 25 April,2020";
-    // Insurance Relief
-  } else if (annuallmt === "ksh 60000") {
-    return print = "Effective since 1 January 2007";
-    // Allowable Pension Fund Contribution
-  } else if (annuallmt === "ksh 240000") {
-    return print = "Effective since 1 January,2006";
-    // Allowable HOSP Contribution
-  } else if (annuallmt === "ksh 0") {
-    return print = "Effective since 1 January,2021";
-    // Affordable Housing Relief
-  } else if (annuallmt === "ksh 108000") {
-    return print = "Effective since 1 January 2018";
-    // Allowable Owner Occupier Interest 
-  } else if (annuallmt === "ksh 300000") {
-    return print = "Effective since 1 January,2017";
-    // Disability Exemption 
-  } else if (annuallmt === "ksh 1800000") {
-    return print = "Effective since 10 March,2018";
-  }
-}
-  console.log(netSalaryCalculator("ksh 300000"));
+// // Personal Relief
+//   if (annuallmt === "ksh 28800") {
+//     return print = "Effective since 25 April,2020";
+//     // Insurance Relief
+//   } else if (annuallmt === "ksh 60000") {
+//     return print = "Effective since 1 January 2007";
+//     // Allowable Pension Fund Contribution
+//   } else if (annuallmt === "ksh 240000") {
+//     return print = "Effective since 1 January,2006";
+//     // Allowable HOSP Contribution
+//   } else if (annuallmt === "ksh 0") {
+//     return print = "Effective since 1 January,2021";
+//     // Affordable Housing Relief
+//   } else if (annuallmt === "ksh 108000") {
+//     return print = "Effective since 1 January 2018";
+//     // Allowable Owner Occupier Interest 
+//   } else if (annuallmt === "ksh 300000") {
+//     return print = "Effective since 1 January,2017";
+//     // Disability Exemption 
+//   } else if (annuallmt === "ksh 1800000") {
+//     return print = "Effective since 10 March,2018";
+//   }
+// }
+//   console.log(netSalaryCalculator("ksh 300000"));
 
 
 
-// NHIF
-// NHIF rates in effect from 1 April 2015:
+// // NHIF
+// // NHIF rates in effect from 1 April 2015:
 
 
 // When the netSalaryCalculator is declared it checks the grosspay
@@ -159,31 +160,31 @@ console.log(netSalaryCalculator(100000));
 
 
 
-// NSSF RATES 
-//    NEW RATES
+// // NSSF RATES 
+// //    NEW RATES
 
-function netSalaryCalculator(pensionablepay) {
-    if (pensionablepay <= 6000) {
-        return print = "Tier:1"
-    } else if (pensionablepay >=6001 && pensionablepay <= 18000) {
-        return print = "Tier:2"
-    }
-} 
-console.log(netSalaryCalculator(17000));
-
-
-        //  NSSF OLD RATES
+// function netSalaryCalculator(pensionablepay) {
+//     if (pensionablepay <= 6000) {
+//         return print = "Tier:1"
+//     } else if (pensionablepay >=6001 && pensionablepay <= 18000) {
+//         return print = "Tier:2"
+//     }
+// } 
+// console.log(netSalaryCalculator(17000));
 
 
-function netSalaryCalculator(maxcontribution) {
-    if (maxcontribution === 400) {
-        return print = "Pay Monthly";
-    } else if (maxcontribution === 200) {
-        return print = "Pay Biweekly";
-    } else if (maxcontribution === 100) {
-        return print = "Pay Weekly";
-    }
-}
-   console.log(netSalaryCalculator(400));
+//         //  NSSF OLD RATES
+
+
+// function netSalaryCalculator(maxcontribution) {
+//     if (maxcontribution === 400) {
+//         return print = "Pay Monthly";
+//     } else if (maxcontribution === 200) {
+//         return print = "Pay Biweekly";
+//     } else if (maxcontribution === 100) {
+//         return print = "Pay Weekly";
+//     }
+// }
+//    console.log(netSalaryCalculator(400));
 
 
